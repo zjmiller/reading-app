05d263eb461cce488f92c9ab1f571b6ffaa30e95
Zachary Miller <zachary.john.miller@gmail.com>
2017-11-28 11:37:34 -0700

    Add pronunciation audio

--- src/reducers/words.js
+++ src/reducers/words.js
@@ -1,19 +1,26 @@
 import shortid from 'shortid';
 
+import catAudioUrl from '../assets/sounds/cat.m4a';
+import dogAudioUrl from '../assets/sounds/dog.m4a';
+import foxAudioUrl from '../assets/sounds/fox.m4a';
+
 const initialState = [
 	{
 		id: shortid.generate(),
 		word: 'cat',
+		audio: catAudioUrl,
 	},
 	
 	{
 		id: shortid.generate(),
 		word: 'dog',
+		audio: dogAudioUrl,
 	},
 	
 	{
 		id: shortid.generate(),
 		word: 'fox',
+		audio: foxAudioUrl,
 	},
 ];
 
