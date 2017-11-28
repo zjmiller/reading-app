59c553d3da4d182161421a9f8be019748993dd04
Zachary Miller <zachary.john.miller@gmail.com>
2017-11-28 11:23:48 -0700

    Add basic redux store and connect to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+		const { test } = this.props;
+		
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        { test }
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  test: state.test,
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,3 @@
+export default function rootReducer(state = {test: 1}, action) {
+	return state;
+}
