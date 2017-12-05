2fef98622f03c147d43dee84296827e1d5359460
Zachary Miller <zachary.john.miller@gmail.com>
2017-12-04 18:17:25 -0700

    Prevent crash after spelling all words

--- src/selectors/getNewRandomWord.js
+++ src/selectors/getNewRandomWord.js
@@ -5,7 +5,7 @@ export default function getNewRandomWord(state) {
 	const wordsSoFar = state.session.wordsSoFar;
 	
 	let wordsPool;
-	if (words.length === wordsSoFar.length) wordsPool = words;
+	if (words.length <= wordsSoFar.length) wordsPool = words;
 	else wordsPool = words.filter(word => wordsSoFar.indexOf(word.word) === -1);
 
 	const randomIndex = getRandomInteger(0, wordsPool.length - 1);
