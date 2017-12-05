39e71409922d0aee60f35c7e64b507f019867c8c
Zachary Miller <zachary.john.miller@gmail.com>
2017-12-04 18:24:41 -0700

    2 second cooldown after playing audio

--- src/components/WordAudioIcon.js
+++ src/components/WordAudioIcon.js
@@ -2,27 +2,38 @@ import React, { Component } from 'react';
 
 import speakerIconImgSrc from '../assets/images/speaker-icon.svg';
 
-function playAudio(url) {
-	const audio = new Audio(url);
-	audio.play();
-}
-
 class WordAudioIcon extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			playingAudio: false,
+		};
+	}
+	
+	playAudio = url => {
+		if (this.state.playingAudio) return;
+		const audio = new Audio(url);
+		audio.play();
+		this.setState({ playingAudio: true });
+		setTimeout(() => { this.setState({ playingAudio: false })}, 2000);
+	}
+	
   render() {
 		const { word } = this.props;
 		
     return (
       <div 
-				onClick={() => playAudio(word && word.audio)}
+				onClick={() => this.state.playingAudio ? '' : this.playAudio(word && word.audio)}
 				style={{
 					alignItems: 'center',
 					background: 'rgba(250, 250, 251, 0.95)',
 					boxShadow: '0 0 10px #000',
 					borderRadius: '30px',
-					cursor: 'pointer',
+					cursor: this.state.playingAudio ? 'auto' : 'pointer',
 					display: 'flex',
 					height: '60px',
 					justifyContent: 'center',
+					opacity: this.state.playingAudio ? 0.5 : 1,
 					userSelect: 'none',
 					width: '60px',
 				}}
