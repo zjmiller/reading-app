003e9afc0919d1f9d8584ebcda2cc7d2f34fde88
Zachary Miller <zachary.john.miller@gmail.com>
2017-12-07 09:42:41 -0700

    Clean up store creation module

--- src/store/store.js
+++ src/store/store.js
@@ -5,8 +5,10 @@ import thunk from 'redux-thunk'
 import fetchState from '../api/fetchState';
 import saveState from '../api/saveState';
 import items from '../content/items';
+import words from '../content/words';
 import rootReducer from '../reducers/rootReducer';
 
+// handling versioning
 if (localStorage.getItem('READING_APP_V') !== '2') {
 	localStorage.removeItem('READING_APP_STATE');
 	localStorage.setItem('READING_APP_V', '2');
@@ -21,20 +23,22 @@ const modifiedState = {
 	letters: savedState.letters,
 	rewardCards: savedState.rewardCards,
 	session: savedState.session,
+	words,
 }
 
-const store = createStore(rootReducer, modifiedState, applyMiddleware(thunk, createLogger({ collapsed: true })));
+const store = createStore(
+	rootReducer,
+	modifiedState,
+	applyMiddleware(thunk, createLogger({ collapsed: true }))
+);
 
+// this is here so that if user closes browser window while answer modal is
+// showing, but the timer to hide the answer modal doesn't finish counting down
+// before the refresh, then the answer modal isn't stuck open after the user
+// reopens the window
 if (store.getState().session.showingAnswerModal) {
 	store.dispatch({
 		type: 'HIDE_ANSWER_MODAL',
-	});
-}
-
-if (!store.getState().session.items.find(item => item.itemId === 'item-1')) {
-	store.dispatch({
-		type: 'BUY_ITEM_AT_STORE',
-		itemId: 'item-1',
 	});
 }
 
